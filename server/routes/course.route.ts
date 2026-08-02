506c3e51319e4c842af38639d5c88b5a1db096c7
sheraz61 <hsheraz271@gmail.com>
2026-08-02 16:36:30 +0500

    add route and controller for retrieving course content for valid users

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { authorizeRoles, isAuthenticated } from "../middleware/auth.js";
-import { editCourse, getAllCourses, getSingleCourse, uploadCourse } from "../controllers/course.controller.js";
+import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller.js";
 
 const router = express.Router();
 router.post(
@@ -23,6 +23,11 @@ router.get(
 router.get(
   "/get-courses",
   getAllCourses,
+);
+router.get(
+  "/get-course-content/:id",
+  isAuthenticated,
+  getCourseByUser,
 );
 
 
