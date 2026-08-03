bd2b09029e2719e54ee303b3092130ad167ceb9f
sheraz61 <hsheraz271@gmail.com>
2026-08-03 11:09:22 +0500

    add review reply controller

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 import { authorizeRoles, isAuthenticated } from "../middleware/auth.js";
-import { addAnwser, addQuestion, addReview, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller.js";
+import { addAnwser, addQuestion, addReplyToReview, addReview, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller.js";
 
 const router = express.Router();
 router.post(
@@ -43,6 +43,12 @@ router.put(
   "/add-review/:id",
   isAuthenticated,
   addReview,
+);
+router.put(
+  "/add-reply",
+  isAuthenticated,
+  authorizeRoles("admin"),
+  addReplyToReview,
 );
 
 
