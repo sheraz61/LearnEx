21b5a1ec6330b9d7383e0bbcbcecfdd0f3dee196
sheraz61 <hsheraz271@gmail.com>
2026-08-18 00:49:38 +0500

    fix the hero img error by adding static files and fix the typo of order analytics page...

--- admin/app/components/Admin/Analytics/OrdersAnalytics.tsx
+++ admin/app/components/Admin/Analytics/OrdersAnalytics.tsx
@@ -25,7 +25,7 @@ export default function OrdersAnalytics({ isDashboard }: Props) {
 
   data &&
     data.orders.last12Months.forEach((item: any) => {
-      analyticsData.push({ name: item.name, Count: item.count });
+      analyticsData.push({ name: item.month, Count: item.count });
     });
 
   return (
