0189995858c412b4481342fffdc4c193da91a622
sheraz61 <hsheraz271@gmail.com>
2026-08-19 22:23:57 +0500

    cross origin production issue

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -23,14 +23,14 @@ interface ITokenOptions {
     expires: new Date(Date.now() + accessTokenExpire * 60 * 1000),
     maxAge: accessTokenExpire * 60 * 1000,
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
   export const refreshTokenOptions: ITokenOptions = {
-    expires: new Date(Date.now() + refreshTokenExpire *24*60*60* 1000),
-    maxAge: refreshTokenExpire*24 *60*60* 1000,
+    expires: new Date(Date.now() + refreshTokenExpire * 24 * 60 * 60 * 1000),
+    maxAge: refreshTokenExpire * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
   };
 
 export const  sendToken = async (user: IUser, statusCode: number, res: Response) => {
@@ -45,6 +45,7 @@ await redis.set(user._id.toString(),JSON.stringify(user)  as any)
   // only set secure flag in production
   if (process.env.NODE_ENV === 'production') {
     accessTokenOptions.secure = true;
+    refreshTokenOptions.secure = true;
   }
 
   res.cookie('access_token', accessToken, accessTokenOptions);
